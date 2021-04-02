1842b51b89234bd535a77e2664ded02eb5f5c70e
Tibor Kiss <kiss.tibor@gmail.com>
2021-04-02 21:57:22 +0200

    stt setup and run
    without installing the module with pip install .

--- run.bat
+++ run.bat
@@ -1,4 +1,4 @@
 cd rg_speech_to_text
-start venv/Scripts/python examples/interface/client_demo.py
+start rg_speech_to_text\venv\Scripts\python TheSoundOfAIOSR\stt\interface\server_app.py
 cd ..
 rem start ./rg_production.bat
