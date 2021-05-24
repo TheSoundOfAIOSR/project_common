03145f7f876dcd1785670ed33dbf1d3ddf8ed661
Jesse Hill <j_hill14@u.pacific.edu>
2021-05-24 16:15:39 -0400

    Run TTS

--- run.bat
+++ run.bat
@@ -4,4 +4,5 @@ cd ..
 cd rg_sound_generation\sound_generator
 start venv\Scripts\python main_rgws.py
 cd ../..
-start docker run --name tts_image -ti --rm tts_image
+cd rg_text_to_sound
+bash -c "source venv/bin/activate; cd tts_websocketserver/src; python -m tts_websocketserver.tts_server"
