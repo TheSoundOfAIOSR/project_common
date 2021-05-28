5f398a8522369a36342aa85fea6b66bbdefc44fd
wass <wassim.filali@gmail.com>
2021-05-28 18:43:48 +0200

    updating 3.8

--- setup.bat
+++ setup.bat
@@ -1,7 +1,6 @@
 python utils\check_version.py || Exit /b
 echo "1/4 Installing Speech to text"
 cd rg_speech_to_text
-git pull origin main
 python -m venv venv
 venv\Scripts\python -m pip install --upgrade pip setuptools
 venv\Scripts\python -m pip install torch==1.7.1+cpu torchvision==0.8.2+cpu torchaudio==0.7.2 -f https://download.pytorch.org/whl/torch_stable.html
@@ -13,22 +12,20 @@ venv\Scripts\python -m pip install -r requirements.txt
 cd ..
 echo "2/4 Installing Sound Generation"
 cd rg_sound_generation\sound_generator
-git pull origin main
 python -m venv venv
 venv\Scripts\python -m pip install --upgrade pip setuptools
 venv\Scripts\python -m pip install -r requirements.txt
 venv\Scripts\python -m pip install requests
 venv\Scripts\python sound_generator\download_checkpoints.py
+rem venv\Scripts\garden.bat install knob
 cd ..\..
 echo "3/4 Installing Production"
 cd rg_production
-git pull origin main
 python -m venv venv
 venv\Scripts\python -m pip install --upgrade pip setuptools
 venv\Scripts\python -m pip install -r requirements.txt
 cd ..
 echo "4/4 Installing Text to sound"
 cd rg_text_to_sound
-git pull origin main
 bash -c "python3 -m venv venv; source venv/bin/activate; pip install --upgrade pip setuptools; pip install rgws; pip install git+https://git@github.com/TheSoundOfAIOSR/rg_text_to_sound.git#'subdirectory=tts_pipeline';"
 cd ..
--- setup3.8.bat
+++ setup3.8.bat
@@ -1,14 +1,34 @@
 python3.8 utils\check_version.py || Exit /b
+echo "1/4 Installing Speech to text"
 cd rg_speech_to_text
 python3.8 -m venv venv
-rem venv\Scripts\activate : activating venv breaks the bat file execution
 venv\Scripts\python -m pip install --upgrade pip setuptools
 venv\Scripts\python -m pip install torch==1.7.1+cpu torchvision==0.8.2+cpu torchaudio==0.7.2 -f https://download.pytorch.org/whl/torch_stable.html
-venv\Scripts\python -m pip install -r requirements-win.txt
+venv\Scripts\python -m pip install sounddevice
+venv\Scripts\python -m pip install pipwin
+venv\Scripts\pipwin install pyaudio
+venv\Scripts\python -m pip install sounddevice
+venv\Scripts\python -m pip install -r requirements.txt
 cd ..
+echo "2/4 Installing Sound Generation"
+cd rg_sound_generation\sound_generator
+python3.8 -m venv venv
+venv\Scripts\python -m pip install --upgrade pip setuptools
+venv\Scripts\python -m pip install -r requirements.txt
+venv\Scripts\python -m pip install requests
+venv\Scripts\python sound_generator\download_checkpoints.py
+rem venv\Scripts\garden.bat install knob
+cd ..\..
+echo "3/4 Installing Production"
 cd rg_production
 python3.8 -m venv venv
 venv\Scripts\python -m pip install --upgrade pip setuptools
 venv\Scripts\python -m pip install -r requirements.txt
-venv\Scripts\garden.bat install knob
+cd ..
+echo "4/4 Installing Text to sound"
+cd rg_text_to_sound
+python3.8 -m venv venv
+venv\Scripts\pip install --upgrade pip setuptools
+venv\Scripts\pip pip install rgws
+venv\Scripts\pip install git+https://git@github.com/TheSoundOfAIOSR/rg_text_to_sound.git#'subdirectory=tts_pipeline'
 cd ..
