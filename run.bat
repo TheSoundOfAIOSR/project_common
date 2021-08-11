1d9d72cc0e9f64a82a0c556a25c2ebc48202998b
MKDYasserH <khalafaoui.yasser@gmail.com>
2021-08-11 17:29:32 +0200

    modified run and setup batch files for tts pipeline

--- run.bat
+++ run.bat
@@ -4,7 +4,6 @@ cd ..
 cd rg_sound_generation
 start venv\Scripts\python main_rgws.py
 cd ..
-cd rg_text_to_sound/tts_websocketserver/src
-REM bash -c "source venv/bin/activate; cd tts_websocketserver/src; python -m tts_websocketserver.tts_server"
-start venv/Scripts/python -m tts_websocketserver/tts_server
+cd rg_text_to_sound
+start venv/Scripts/python -m tts_websocketserver/src/tts_websocketserver/tts_server
 cd ..
