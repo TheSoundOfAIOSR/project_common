915b264a2e8a1db3c99497772e86e4d11ec7971a
Jesse Hill <j_hill14@u.pacific.edu>
2021-08-19 17:09:05 -0400

    Correct command to start up tts

--- run.bat
+++ run.bat
@@ -4,6 +4,6 @@ cd ..
 cd rg_sound_generation
 start venv\Scripts\python main_rgws.py
 cd ..
-cd rg_text_to_sound
-start venv/Scripts/python -m tts_websocketserver/src/tts_websocketserver/tts_server
-cd ..
+cd rg_text_to_sound\tts_websocketserver\src
+start ..\..\venv\Scripts\python -m tts_websocketserver.tts_server
+cd ..\..\..
